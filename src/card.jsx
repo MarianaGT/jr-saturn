7683144d51760e550ba214473a04353a2e90497a
Mariana Galan <gatm870125@gmail.com>
2021-02-11 10:34:29 -0600

    note on card class skeleton

--- src/card.jsx
+++ src/card.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 class Card extends Component {
   render() {
     return (
+      //multiple elements can be returned with React.Fragment
       <React.Fragment>
         <Element1 />
         <Element2 />
