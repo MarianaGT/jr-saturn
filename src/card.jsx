99a7f81fe75abc0b58ad370e74420cd0e0420e99
Mariana Galan <gatm870125@gmail.com>
2021-02-11 10:33:54 -0600

    card class skeleton

---
+++ src/card.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Card extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Element1 />
+        <Element2 />
+      </React.Fragment>
+    );
+  }
+}
