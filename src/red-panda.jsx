9c6d61207c08826e2630d3be1e3fd2a37b4810d5
Mariana Galan <gatm870125@gmail.com>
2021-02-11 11:03:14 -0600

    red panda

---
+++ src/red-panda.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const panda = {
+  title: 'Cute Red Panda',
+  src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Endangered_Red_Panda.jpg',
+  alt: 'Red Panda',
+  width: '200px'
+};
+
+class RedPanda extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{panda.title}</h1>
+        <img
+        src={panda.src}
+        alt={panda.alt}
+        width={panda.width}/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <RedPanda />,
+  document.getElementById('panda')
+);
