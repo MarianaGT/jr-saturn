1f0dede89022bca0efb64d02ad162057c69c4a0c
Mariana Galan <gatm870125@gmail.com>
2021-02-11 10:39:57 -0600

    note on creating more than one component instance at a time

--- src/card.jsx
+++ src/card.jsx
@@ -17,3 +17,18 @@ const card = document.getElementById('card');
 if (card) {
   ReactDOM.render(<Card />, card);
 }
+
+/* When running the ReactDOM.render() method, you can only pass in one value as
+the first argument, which is the component to be rendered. However, you can create
+more than one component instance at a time, by simply wrapping them inside of
+another element, such as a <div>. For example, you could do the following to render two components,
+
+ReactDOM.render(
+  <div>
+    <ComponentOne />
+    <ComponentTwo />
+  </div>,
+  document.getElementById('app')
+);
+
+Another option is to create another component for the purpose of wrapping multiple components to render.*/
