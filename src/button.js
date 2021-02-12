ffcbc4edec323126c4c7dcb00887f4e0d86b020d
Mariana Galan <gatm870125@gmail.com>
2021-02-12 16:54:00 -0600

    button component

--- src/button.js
+++ src/button.js
@@ -1,1 +1,13 @@
-button.js
+import React from 'react';
+
+export class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className={ this.props.light ? 'light-button' : 'dark-button' }
+        onClick={this.props.onClick}>
+        Refresh
+      </button>
+    );
+  }
+}
